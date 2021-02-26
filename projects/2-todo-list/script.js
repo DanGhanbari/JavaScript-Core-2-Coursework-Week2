dbb00c337ff39ed6580627a96e1c103c16997c28
Dan <danielghanbari7@gmail.com>
2021-02-26 18:50:25 +0000

    2-todo-list-project

--- projects/2-todo-list/script.js
+++ projects/2-todo-list/script.js
@@ -1,6 +1,48 @@
 function populateTodoList(todos) {
-  let list = document.getElementById("todo-list");
   // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
+  let list = document.getElementById("todo-list");
+
+  for (let i = 0; i < todos.length; i++) {
+    // Create list Elements and add class name to it
+    const liEl = document.createElement("li");
+    liEl.className =
+      "list-group-item d-flex justify-content-between align-items-center";
+
+    // Create paragraph element and add text to it
+    const pEl = document.createElement("p");
+    pEl.innerText = todos[i].task;
+
+    // Create span
+    const spanEl = document.createElement("span");
+    spanEl.className = "badge bg-primary rounded-pill";
+    const trashBtn = document.createElement("i");
+    trashBtn.className = "fa fa-trash";
+    const checkBtn = document.createElement("i");
+    checkBtn.className = "fa fa-check";
+
+    // Adding event listener to check button
+    checkBtn.addEventListener("click", () => {
+      if (liEl.style.textDecoration === "line-through") {
+        todos[i].completed = false;
+        liEl.style.textDecoration = "";
+      } else {
+        todos[i].completed = true;
+        liEl.style.textDecoration = "line-through";
+      }
+    });
+
+    // Adding event listener to trash button
+    trashBtn.addEventListener("click", () => {
+      liEl.remove();
+    });
+
+    // Append all elements
+    liEl.appendChild(pEl);
+    spanEl.appendChild(checkBtn);
+    spanEl.appendChild(trashBtn);
+    liEl.appendChild(spanEl);
+    list.appendChild(liEl);
+  }
 }
 
 // These are the same todos that currently display in the HTML
@@ -17,9 +59,38 @@ function addNewTodo(event) {
   // The code below prevents the page from refreshing when we click the 'Add Todo' button.
   event.preventDefault();
   // Write your code here... and remember to reset the input field to be blank after creating a todo!
+  const inputEl = document.getElementById("todoInput");
+  let list = document.getElementById("todo-list");
+  if (inputEl.value) {
+    let object = {};
+    object.task = inputEl.value;
+    object.completed = false;
+    todos.push(object);
+    let newObj = todos.splice(todos.length - 1);
+    populateTodoList(newObj);
+    inputEl.value = "";
+  }
 }
 
 // Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
-function deleteAllCompletedTodos() {
-  // Write your code here...
+function deleteAllCompletedTodos(event) {
+  // Create new button called "delete"
+  const p = document.createElement("p");
+  p.innerText = "Delete";
+  const newButton = document.createElement("button");
+  newButton.className = "btn btn-primary mb-3";
+  const buttonDiv = document.getElementsByClassName("card-body")[0];
+  newButton.appendChild(p);
+  buttonDiv.appendChild(newButton);
+
+  // Adding event listener to "delete" button
+  newButton.addEventListener("click", () => {
+    let liEl = document.getElementsByClassName("list-group-item");
+    for (let i = 0; i < liEl.length; i++) {
+      if (liEl[i].style.textDecoration === "line-through") {
+        liEl[i].parentNode.removeChild(liEl[i]);
+      }
+    }
+  });
 }
+deleteAllCompletedTodos();
